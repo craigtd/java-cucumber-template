9f7269a6f1c6b134aedaefdd91777603367b9cd0
Craig Duncan <craig.duncan@digital.hmrc.gov.uk>
2017-10-12 20:34:27 +0100

    Added Hooks to initialise browser and tear down after each test

--- src/test/java/org/testcraft/stepDefs/GoogleSearchStepdefs.java
+++ src/test/java/org/testcraft/stepDefs/GoogleSearchStepdefs.java
@@ -3,9 +3,19 @@ package org.testcraft.stepDefs;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 public class GoogleSearchStepdefs {
 
-    //TODO: Implement step definitions for the scenario steps
+    public WebDriver driver;
+
+    public GoogleSearchStepdefs() {
+        driver = Hooks.driver;
+    }
+
+    @Given("^I'm on google\\.co\\.uk main page$")
+    public void openGoogleMainPage() {
+        driver.get("http://www.google.co.uk");
+    }
 
 }
