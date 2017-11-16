b42cce6067b0dd744f5c32aa6f15e1522053879a
Craig Duncan <craig.duncan@digital.hmrc.gov.uk>
2017-11-16 20:13:29 +0000

    [CD] Added Hamcrest matchers

---
+++ src/test/java/uk/co/testcraft/pages/GooglePage.java
@@ -0,0 +1,54 @@
+package uk.co.testcraft.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class GoogleHomePage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(name = "q")
+    private WebElement searchBox;
+
+    @FindBy(name = "btnK")
+    private WebElement searchButton;
+
+    @FindBy(className = "_Rm")
+    private List<WebElement> searchResults;
+
+
+    public GoogleHomePage(final WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, 5);
+    }
+
+    public void goTo() {
+        this.driver.get("https://www.google.com");
+    }
+
+    public void searchFor(String criteria) {
+        this.searchBox.sendKeys(criteria);
+        wait.until(ExpectedConditions.elementToBeClickable(this.searchButton));
+        // Now submit the form. WebDriver will find the form for us from the element searchBox
+//        this.searchBox.submit();
+        this.searchButton.click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("rc")));
+    }
+
+    public List<WebElement> getResults() {
+        return this.searchResults;
+    }
+
+    public void quit() {
+        this.driver.quit();
+    }
+}
